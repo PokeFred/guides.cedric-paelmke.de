ed227d46a806c2a57e7e1aba1fcbb67ddff08521
PokeFred <Cedric.paelmke@gmx.de>
2024-02-28 20:38:18 +0100

    add skeleton ui

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,15 @@
 <script lang="ts">
     import '../app.css'
+    import { writable } from 'svelte/store'
+    import type { Writable } from 'svelte/store'
+
+    type Theme = 'skeleton'
+    const theme: Writable<Theme> = writable<Theme>('skeleton')
+    const darkmode: Writable<boolean> = writable<boolean>(true)
 </script>
 
-<slot />
+<body data-theme={$theme} class={$darkmode ? 'dark' : 'light'}>
+    <div class="bg-surface-100-800-token text-surface-900-50-token relative h-screen w-screen overflow-hidden p-4">
+        <slot />
+    </div>
+</body>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
+import { skeleton } from '@skeletonlabs/tw-plugin'
+import { join as joinPath } from 'path'
+
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ['./src/app.html', './src/routes/**/*.svelte', './src/lib/components/**/*.svelte'],
+    darkMode: 'class',
+    content: ['./src/app.html', './src/routes/**/*.svelte', './src/lib/components/**/*.svelte', joinPath(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,svelte,js,ts}')],
     theme: {
         extend: {},
     },
-    plugins: [],
+    plugins: [
+        skeleton({
+            themes: {
+                preset: ['skeleton'],
+            },
+        }),
+    ],
 }
