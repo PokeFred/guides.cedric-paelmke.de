94cc360ab0b2b657aebf5d9ad4204c5743545ff0
PokeFred <Cedric.paelmke@gmx.de>
2024-02-28 21:26:27 +0100

    update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,10 @@
 <script lang="ts">
     import '../app.css'
-    import { writable } from 'svelte/store'
-    import type { Writable } from 'svelte/store'
-
-    type Theme = 'skeleton'
-    const theme: Writable<Theme> = writable<Theme>('skeleton')
-    const darkmode: Writable<boolean> = writable<boolean>(true)
+    import Darkmode from "$stores/darkmode"
+    import Theme from "$stores/theme"
 </script>
 
-<body data-theme={$theme} class={$darkmode ? 'dark' : 'light'}>
+<body data-theme={$Theme} class={$Darkmode ? 'dark' : 'light'}>
     <div class="bg-surface-100-800-token text-surface-900-50-token relative h-screen w-screen overflow-hidden p-4">
         <slot />
     </div>
